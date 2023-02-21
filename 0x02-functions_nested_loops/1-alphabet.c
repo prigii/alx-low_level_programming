e3edcca6de9ad5fa6b8f873f7b48f98517fcab1f
Peter Rigii <prigii@PETER-LEO>
2023-02-21 21:12:13 +0300

    printing the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 /**
  ** print_alphabet - prints out the alphabet
  ** main - uses built in function
- ** Return: 0 if succesful
+ ** Return: 0 if successful
  **/
 
 void print_alphabet(void)
 {
 	char i;
+	
 	for (i = 'a'; i <= 'z'; i++)
-
 	_putchar(i);
 	_putchar('\n');
 }
