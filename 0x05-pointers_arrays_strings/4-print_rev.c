4e0bcd62bb51cd4c2748317ec1bee4ae399548b9
Peter Rigii <theonlyrigii@gmail.com>
2023-02-27 20:25:42 +0300

    reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
- #include "main.h"
- /**
-  * print_rev - a function that prints a string, in reverse
-  * fcounter is to first count to end, n is to count back
-  * @s: str input
-  * Return: string in reverse
-  */
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
+ */
 void print_rev(char *s)
 {
 int fcounter = 0;
