edbfaafe356d3186487d62f7eadfa95a32dbad9b
Peter Rigii <theonlyrigii@gmail.com>
2023-02-27 20:24:25 +0300

    reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,17 +5,17 @@
   * @s: str input
   * Return: string in reverse
   */
- void print_rev(char *s)
- {
- int fcounter = 0;
- int i, n;
- for (i = 0; s[i] != '\0'; i++)
- {
- fcounter++;
- }
- for (n = (fcounter - 1); n >= 0; n--)
- {
- _putchar(s[n]);
- }
- _putchar('\n');
- }
+void print_rev(char *s)
+{
+int fcounter = 0;
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
+{
+fcounter++;
+}
+for (n = (fcounter - 1); n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
+}
