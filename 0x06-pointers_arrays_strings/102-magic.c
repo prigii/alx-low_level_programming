b31e86ee627d674a6a96bad85680e07045512ced
Peter Rigii <theonlyrigii@gmail.com>
2023-03-02 23:15:08 +0300

    edited task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -8,7 +16,7 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/**
+/*
  *write your line of code here...
  *Remember:
  *-you are not allowed to use a
