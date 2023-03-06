e432ccaff5eda5fd78a37803825a35dddd63b093
Peter Rigii <theonlyrigii@gmail.com>
2023-03-06 16:03:13 +0300

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
+char *dest1 = dest;
+char *src1 = src;
 
-for (i = 0; i < n; i++)
-*(dest + i) =  *(src + i);
-
+for (; n != 0; dest1++, n--, src1++)
+*dest1 = *src1;
 return (dest);
 }
