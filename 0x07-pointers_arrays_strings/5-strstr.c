600d41e63371a27903c7d1b2214c13ba523a1626
Peter Rigii <theonlyrigii@gmail.com>
2023-03-06 17:35:18 +0300

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,24 +14,17 @@ int i;
 int s = 0;
 while (needle[s] != '\0')
 s++;
-
 while (*haystack)
-
 {
 for (i = 0; needle[i]; i++)
-
 {
 if (haystack[i] != needle[i])
 break;
-
 }
 if (i != s)
 haystack++;
-
 else
-
 return (haystack);
-
 }
 return (NULL);
 }
