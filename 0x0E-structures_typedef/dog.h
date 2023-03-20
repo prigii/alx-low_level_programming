1cd88e747a4329e26fd7d5432aebcf208f9608ef
Peter Rigii <theonlyrigii@gmail.com>
2023-03-20 18:29:15 +0300

    changed the dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
  *  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+
 /**
- *  * struct dog - structure for dogs
- *   * @name: first member
- *    * @age: second member
- *     * @owner: third member
+ *  * struct dog - struct that stores some information of a dog
+ *   * @name: name of the dog
+ *    * @age: age of the dog
+ *     * @owner: owner of the dog
+ *      *
+ *       * Description: struct called "dog" that stores its name, its age
+ *        * and the name of its owner.
  */
 struct dog
 {
@@ -16,8 +21,10 @@ char *name;
 float age;
 char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
