560c1138069c04890d2c8a4741833d9cd778edb7
Peter Rigii <theonlyrigii@gmail.com>
2023-03-22 12:14:55 +0300

    creates a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include "stdlib.h"
 
 /**
  *  * print_name - prints a name.
@@ -9,6 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
+if (name != NULL && f != NULL)
 f(name);
 }
