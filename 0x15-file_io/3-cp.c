6649618a408b36ae583c3c7723171a23df194651
Peter Rigii <peterree@DESKTOP-STIF1CN>
2023-04-11 11:45:08 +0300

    edited 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file%\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
 exit(98);
 }
 if (file_to == -1)
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 int file_from, file_to, err_close;
 ssize_t nchars, nwr;
 char buf[1024];
-if (argv != 3)
+if (argc != 3)
 {
 dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 exit(97);
@@ -45,7 +45,7 @@ while (nchars == 1024)
 {
 nchars = read(file_from, buf, 1024);
 if (nchars == -1)
-error_file(-1, 0, argc);
+error_file(-1, 0, argv);
 nwr = write(file_to, buf, nchars);
 if (nwr == -1)
 error_file(0, -1, argv);
