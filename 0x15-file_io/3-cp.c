7f7f35f4035a577053e5f8e2406168e426ad5705
Peter Rigii <theonlyrigii@gmail.com>
2023-04-11 15:38:54 +0300

    edited 3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  * @argv: arguments vector
  * Return: null
  */
-void error_file(int file_from, int file_to, char **argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 if (file_from == -1)
 {
